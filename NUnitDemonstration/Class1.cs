437bc90455cf8d9b4c75c15a75c016bf05596508
Captain <Captain@DESKTOP-DRATU48>
2023-10-11 13:24:30 -0400

    Add project files.

---
+++ NUnitDemonstration/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NUnitDemonstration
+{
+    public static class Triangle
+    {
+        public static string ValidTriangle(int firstAngle, int secondAngle, int
+        thirdAngle)
+        {
+            string result;
+            if ((firstAngle + secondAngle + thirdAngle) == 180) 
+            {
+                result = "THE TRIANGLE IS VALID.";
+            }
+            else
+            {
+                result = "THE TRIANGLE CREATED BY (VISHAL THAKOR) IS NOT VALID.";
+            }
+            return result;
+        }
+    }
+}
+
---
+++ TestClass/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnitDemonstration;
+
+namespace TriangleTest
+{
+    [TestFixture]
+
+    public class TriangleTest1
+    {
+        [Test]
+
+        public void ValidTriangle_Input60and60and60_OutputValidTriangle()
+        {
+            int firstAngle = 60;
+            int secondAngle = 60;
+            int thirdAngle = 60;
+
+            string expected = "THE TRIANGLE IS VALID.";
+
+            string actual = Triangle.ValidTriangle(firstAngle,secondAngle,thirdAngle);
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
